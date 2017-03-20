47aa46e95cb89c68fcdbe57e99ccbd1a456b8492
Malikov <malikov.yurii.alex@gmail.com>
2017-03-20 23:03:08 +0200

    BaseEntity added

---
+++ src/main/java/com/malikov/freelance/model/BaseEntity.java
@@ -0,0 +1,65 @@
+package com.malikov.freelance.model;
+
+import org.hibernate.Hibernate;
+import org.springframework.data.domain.Persistable;
+
+import javax.persistence.*;
+
+/**
+ * This is parent class for domain classes which has ID
+ * @author Malikov Yurii
+ * @version 0.1
+ */
+@MappedSuperclass
+@Access(AccessType.FIELD)
+//@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, isGetterVisibility = NONE, setterVisibility = NONE)
+public class BaseEntity implements Persistable<Integer> {
+
+    //    @GeneratedValue(generator = "increment")
+//    @GenericGenerator(name = "increment", strategy = "increment")
+    // PROPERTY access for id due to bug: https://hibernate.atlassian.net/browse/HHH-3718
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @Access(value = AccessType.PROPERTY)
+    protected Integer id;
+
+    public BaseEntity() {
+    }
+
+    protected BaseEntity(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return (getId() == null);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || !getClass().equals(Hibernate.getClass(o))) {
+            return false;
+        }
+        BaseEntity that = (BaseEntity) o;
+
+        return null != getId() && getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return (getId() == null) ? 0 : getId();
+    }
+}
